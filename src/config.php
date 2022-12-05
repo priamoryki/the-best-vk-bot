d2281f799d219a09f9cdc3247f5b5cbd3fa83b0b
priamoryki <60192758+priamoryki@users.noreply.github.com>
2022-12-05 03:20:53 +0300

    fix config.php

--- src/config.php
+++ src/config.php
@@ -5,4 +5,4 @@ namespace Bot;
 define("GROUP_ID", getenv("VK_GROUP_ID"));
 define("BOT_TOKEN", getenv("VK_BOT_TOKEN"));
 define("API_CONFIRMATION_TOKEN", getenv("VK_API_CONFIRMATION_TOKEN"));
-define("GROUP_SECRET", getenv("GROUP_SECRET"));
+define("GROUP_SECRET", getenv("VK_GROUP_SECRET"));
